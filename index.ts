08dac9a2687b9d70e2c47f3aff34d225d71b7a17
Joseph A <joseph.atureta@gmail.com>
2021-05-02 17:20:40 +0100

    Added query resolvers

---
+++ index.ts
@@ -0,0 +1,119 @@
+import { ApolloServer } from "apollo-server";
+// import { DateTimeResolver } from "graphql-scalars";
+
+import { PrismaClient } from '@prisma/client';
+// const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
+
+
+const typeDefs = `
+type Query {
+  allUsers: [User!]!
+  postById(id: Int!): Post
+  feed(searchString: String, skip: Int, take: Int): [Post!]!
+  draftsByUser(id: Int!): [Post]
+}
+
+type Mutation {
+  signupUser(name: String, email: String!): User!
+  createDraft(title: String!, content: String, authorEmail: String): Post
+  incrementPostViewCount(id: Int!): Post
+  deletePost(id: Int!): Post
+}
+
+type User {
+  id: Int!
+  email: String!
+  name: String
+  posts: [Post!]!
+}
+
+type Post {
+  id: Int!
+  createdAt: DateTime!
+  updatedAt: DateTime!
+  title: String!
+  content: String
+  published: Boolean!
+  viewCount: Int!
+  author: User
+}
+
+scalar DateTime
+`;
+
+const resolvers = {
+  Query: {
+    allUsers: async (parent, args, context) => {
+      const { prisma } = context;
+      return await prisma.user.findMany();
+    },
+    postById: async (parent, args: { id }, context) => {
+      const { prisma } = context;
+      return await prisma.post.findUnique({
+        where: { id: args.id}
+      })
+    },
+    feed: (parent, args, context) => {
+      // TODO
+    },
+    draftsByUser: async (parent, args: { id }, context) => {
+      const { prisma } = context;
+      return await prisma.user.findUnique({
+        where: { id: args.id}
+      }).posts({
+        where: { published: false } 
+      })
+    },
+  },
+  Mutation: {
+    signupUser: async (parent,args,context) => {
+      const { prisma } = context;
+      return await prisma.user.create({
+        data: {
+          email:args.email,
+          name: args.name
+        }
+      })
+    },
+    createDraft: async (parent, args: { title: string; content: string | undefined; authorEmail: string }, context) => {
+      const { prisma } = context;
+      return await prisma.post.findUnique({})
+    },
+    incrementPostViewCount: ( parent, args, context) => {
+      const { prisma } = context;
+      return prisma.post.findUnique({
+        where: { id: args.id},
+        
+      })
+    },
+    deletePost: async (parent, args, context) => {
+      const { prisma } = context;
+      return await prisma.post.delete({
+        where: { id : args.id}
+      })
+    },
+  },
+  Post: {
+    author: async (parent, args, context) => {
+      const { prisma } = context;
+      return await prisma.post.findUnique({
+        where : { id : parent.id }
+      }).author();
+    },
+  },
+  User: {
+    posts: async (parent, args, context) => {
+      const { prisma } = context;
+      return await prisma.user.findUnique({
+        where: { id: parent.id}
+      }).posts()
+    },
+  },
+  // DateTime: DateTimeResolver,
+};
+
+const server = new ApolloServer({ typeDefs, resolvers, context: () => ({prisma}) });
+server.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at: http://localhost:4000`)
+);
---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+import {PrismaClient} from "@prisma/client";
+// const { PrismaClient } = pkg; 
+const prisma = new PrismaClient();
+
+const userData = [
+  {
+    name: "Joseph",
+    email: "joseph@hhub.com",
+    posts: {
+      create: [
+        {
+          title: "Joint the Hhub Slack",
+          content: "https://slack.hhub.io",
+          published: true
+        }
+      ]
+    }
+  },{
+    name: "Isaac",
+    email: "isaac@hhub.com",
+    posts: {
+      create: [
+        {
+          title: "Follow Hhub on Twitter",
+          content:"https://www.twitter.com/hhub",
+          published: true
+        }
+      ]
+    }
+  }
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
