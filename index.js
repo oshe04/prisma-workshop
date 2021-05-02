cb6f9a0b63ac27bec558449712e462705ce40b80
Joseph A <joseph.atureta@gmail.com>
2021-05-02 01:06:01 +0100

    Tested prisma client with multiple models/tables

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+    const result = await prisma.user.findMany({
+        skip: 2,
+        take: 2,
+    });
+    console.log(result);
+}
 
 main()
     .catch((err) => {
+        console.log(`${err.message}\n`);
         throw err;
     })
     .finally(async () => {
